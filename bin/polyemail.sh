c54df4d949cf7f1d4a72427cf9f81f0c1efd16b2
lostoli <lostoli@gmail.com>
2017-06-05 07:40:44 +0300

    added startpage

--- bin/polyemail.sh
+++ bin/polyemail.sh
@@ -2,7 +2,7 @@
 
 colorm() {
     if [ "$1" -ge "5" ]; then
-        echo %{F#$C13}$1%{F-}
+        echo %{F#$C11}$1%{F-}
         elif [ "$1" -ge "1" ]; then
         echo %{F#$C9}$1%{F-}
         else 
@@ -14,4 +14,4 @@ LO=$(ls /home/oli/Mail/lostoli/INBOX/new | wc -l)
 OG=$(ls /home/oli/Mail/o.g.sargent/INBOX/new | wc -l)
 OS=$(ls /home/oli/Mail/olivers2/INBOX/new | wc -l)
 
-echo "%{F#$C8}Mail:%{F-} $(colorm $LO)|$(colorm $OG)|$(colorm $OS)"
+echo "%{F#$C4}Mail:%{F-} $(colorm $LO)|$(colorm $OG)|$(colorm $OS)"
--- bin/polyupdate.sh
+++ bin/polyupdate.sh
@@ -4,5 +4,5 @@
 if [ "$(cat ~/.cache/updates)" -ge "1" ] ; then
     echo "%{F#$C3}%{F-}"
 else
-    echo "%{F#$Cf}%{F-}"
+    echo "%{F#$C15}%{F-}"
 fi
